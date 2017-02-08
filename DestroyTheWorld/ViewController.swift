e22f1b6bba078f20c9d1606078df510cb847f614
Zbigniew Frysztak <zbigniew.fr@gmail.com>
2017-02-08 12:36:39 +0100

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Zbigniew Frysztak on 08/02/2017.
+//  Copyright © 2017 Zbigniew Frysztak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
